c5969b5244fe283ffd454991ff1173098bc2191c
JiHo Jeon <wjswlgh96@naver.com>
2024-10-28 17:23:29 +0900

    init

---
+++ src/themes/theme.ts
@@ -0,0 +1,17 @@
+// my-theme.ts
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  red: "#E51013",
+  black: {
+    veryDark: "#141414",
+    darker: "#181818",
+    lighter: "#2F2F2F",
+  },
+  white: {
+    lighter: "#FFF",
+    darker: "#E5E5E5",
+  },
+};
+
+export { theme };
